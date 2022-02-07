68e284771adb659abb7a20c052bf766ca49bea24
brightsummit.co <naseemoyebola@gmail.com>
2022-02-07 21:37:15 +0100

    Initial Commit

---
+++ CoreDataProject/CoreDataProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoreDataProjectApp.swift
+//  CoreDataProject
+//
+//  Created by IBE on 07/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct CoreDataProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
