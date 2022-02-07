11689f941587801601715a460fbc5ca78755dd97
brightsummit.co <naseemoyebola@gmail.com>
2022-02-07 22:25:51 +0100

    NSManagedObject

---
+++ Movie+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Movie+CoreDataClass.swift
+//  CoreDataProject
+//
+//  Created by naseem on 07/02/2022.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Movie)
+public class Movie: NSManagedObject {
+
+}
